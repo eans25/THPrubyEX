db8c6aa62630777d03e999f5708bbec497b9aced
eans25 <eangly.eng@gmail.com>
2018-06-13 12:12:49 +0200

    7exo ok -a -b -c dans le même fichier

---
+++ 7exo.rb
@@ -0,0 +1,17 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+#gets.chomp permet à l'utilisateur d'entrer une valeur 
+
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+#ici le programme guide l'utilisateur mais plus précisément, attends une réponse de la part de l'utilisateurqu'il invite à entrer après ">"
+
+
+
+user_name = gets.chomp
+puts user_name
+#le programme ne guide pas l'utilisateur 
+#ici pas de question, l'utilisateur ne sait pas quoi faire
