3927f63fc1821dd937f8b92c7fd190dae110c775
eans25 <eangly.eng@gmail.com>
2018-06-14 10:03:04 +0200

    10exo OK âge que tu avais en 2017

---
+++ 10exo.rb
@@ -0,0 +1,12 @@
+puts "En quelle année es-tu né ?"
+print ">"
+year_birth = gets.chomp
+puts "tu avais donc #{2017 - year_birth.to_i} ans en 2017"
+
+=begin
+year_birth n'est pas une valeur fixé par le programme 
+mais une valeur attendu par l'utilisateur
+to_i permet d'interpréter la valeur donné par l'utilisateur
+
+	
+=end
