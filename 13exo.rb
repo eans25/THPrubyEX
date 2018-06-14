700f428d03cf8ddeaa9f496cc2cf640ab96684c2
eans25 <eangly.eng@gmail.com>
2018-06-14 13:48:00 +0200

    save 13exo

--- 13exo.rb
+++ 13exo.rb
@@ -5,14 +5,15 @@ l'année de naissance d'un utilisateur,
 et qui va ressortir chaque année depuis 
 son année de naissance jusqu'à 2018.
 =end
-puts "En quelle année est-tu née ?"
+puts "En quelle année es-tu née ?"
 print ">"
 date_birthday = gets.chomp.to_i
 n = date_birthday 
+ 
  loop do 
  	puts n 
  	n = n + 1
- 	break if n >2018
+ 	break if n > 2018
  end
 
 
