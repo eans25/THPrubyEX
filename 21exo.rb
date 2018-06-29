0dbcdb22d06d3b01c0633d0ec966c39af711a124
eans25 <eangly.eng@gmail.com>
2018-06-29 03:21:39 +0200

    exo 21 okok

---
+++ 21exo.rb
@@ -0,0 +1,22 @@
+# Semaine 0 - THP - pyramide
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? ATTENTION ma pyramide ne dépasse pas 25 étages!"
+print ">"
+number = gets.chomp.to_i
+n = 1
+
+while number > 25
+	puts "La pyramide ne peut faire plus de 25 étages !!!"
+	puts "Donne moi un nombre compris entre 1 et 25"
+	print ">"
+	number = gets.chomp.to_i
+end
+
+number.times do 
+	puts " " * number + "*" * n
+	n += 1
+	number -= 1
+end
+
+
+
